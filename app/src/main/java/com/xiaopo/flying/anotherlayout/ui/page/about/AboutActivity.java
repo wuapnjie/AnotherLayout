bd96c67728c554853fad7c28367a33929ce19864
wuapnjie <wupanjie0611@gmail.com>
2017-12-01 18:47:36 +0800

    icon

--- app/src/main/java/com/xiaopo/flying/anotherlayout/ui/page/about/AboutActivity.java
+++ app/src/main/java/com/xiaopo/flying/anotherlayout/ui/page/about/AboutActivity.java
@@ -22,19 +22,19 @@ import me.drakeet.support.about.Line;
 public class AboutActivity extends AbsAboutActivity {
 
   @Override
-  protected void onCreateHeader(ImageView icon, TextView slogan, TextView version) {
-    setHeaderContentColor(getResources().getColor(R.color.textColorPrimary));
-    setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
-    icon.setImageResource(R.mipmap.ic_launcher);
+  protected void onCreateHeader(@NonNull ImageView icon, @NonNull TextView slogan, @NonNull TextView version) {
+    setNavigationIcon(R.drawable.ic_close_white_24dp);
+    icon.setImageResource(R.mipmap.ic_launcher_round);
     slogan.setText("Another Layout By wupanjie");
     String versionString = "Version " + BuildConfig.VERSION_NAME;
     version.setText(versionString);
   }
 
+
   @Override
   protected void onItemsCreated(@NonNull Items items) {
-    items.add(new Category("介绍与帮助"));
-    items.add(new Card(getString(R.string.card_content), "分享"));
+    items.add(new Category("另一个布局"));
+    items.add(new Card(getString(R.string.card_content)));
 
     items.add(new Category("Developer"));
     items.add(new Contributor(R.mipmap.ic_launcher, "wupanjie", "Developer & designer",
@@ -63,9 +63,4 @@ public class AboutActivity extends AbsAboutActivity {
         "https://github.com/drakeet/about-page"));
   }
 
-  @Override
-  protected void onActionClick(View action) {
-    super.onActionClick(action);
-
-  }
 }
